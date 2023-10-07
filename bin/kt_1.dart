27fc6ebd844c2eef4cba92b5fbd29e23507f827c
n1k-1ta <n1k_1ta@bk.ru>
2023-10-07 09:50:33 +0500

    last commit

--- bin/kt_1.dart
+++ bin/kt_1.dart
@@ -1,5 +1,19 @@
 import 'package:kt_1/kt_1.dart' as kt_1;
+import 'package:dio/dio.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${kt_1.calculate()}!');
+void main(List<String> arguments) async{
+  Dio client = Dio();
+  String url = 'https://dummyjson.com/products';
+  Response response = await client.get(url);
+
+  Map<String, dynamic> data = response.data;
+  List<dynamic> product = data["products"];
+
+  int count=0;
+  for (var el in product) {
+    if(el["brand"]=='Apple'){
+      count++;
+    }
+  }
+  print(count);
 }
