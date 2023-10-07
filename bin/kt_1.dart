2f8ca3e9f124eba2dbfadfe07bf6cd88c29aaf9e
n1k-1ta <n1k_1ta@bk.ru>
2023-10-07 09:41:12 +0500

    first commit

---
+++ bin/kt_1.dart
@@ -0,0 +1,5 @@
+import 'package:kt_1/kt_1.dart' as kt_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kt_1.calculate()}!');
+}
---
+++ test/kt_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:kt_1/kt_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
